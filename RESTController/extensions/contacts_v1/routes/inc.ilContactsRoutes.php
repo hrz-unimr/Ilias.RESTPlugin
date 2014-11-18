f81cdca977d310eaf7d82abe2a81ebf511c6f52c
Dirk Schäfer <schaefer@hrz.uni-marburg.de>
2014-11-18 11:37:30 +0100

    Revision of the contacts v1 API

--- RESTController/extensions/contacts_v1/routes/inc.ilContactsRoutes.php
+++ RESTController/extensions/contacts_v1/routes/inc.ilContactsRoutes.php
@@ -6,18 +6,26 @@
 $app->group('/v1', function () use ($app) {
 
     /**
-     * Returns the contacts for a user specified by id
+     * Returns the personal ILIAS contacts for a user specified by id.
      */
-    $app->get('/contacts/:id',  function ($id) use ($app) {
-        $result = array();
-
-        $model = new ilContactsModel();
-        $data = $model->getMyContacts($id);
-        // todo: add also group and course contacts:
-        // see ilMailSearchCoursesGUI.php (from Services/Contact/classes)
-        //var_dump($data);
-        $result['msg'] = "Contacts for user ".$id;
-        $result['contacts']['mycontacts'] = $data;
-        echo json_encode($result);
+    $app->get('/contacts/:id', 'authenticate', function ($id) use ($app) {
+        $env = $app->environment();
+        $response = new ilRestResponse($app);
+        $authorizedUserId =  ilRestLib::loginToUserId($env['user']);
+        if ($authorizedUserId == $id || ilRestLib::isAdmin($authorizedUserId)) { // only the user or the admin is allowed to access the data
+            try {
+                $model = new ilContactsModel();
+                $data = $model->getMyContacts($id);
+                $response->setMessage("Contacts for user " . $id . ".");
+                $response->addData('contacts', $data);
+            } catch (Exception $e) {
+                $response->setRestCode("-15");
+                $response->setMessage('Error: Could not retrieve data for user '.$id.".");
+            }
+        } else {
+            $response->setRestCode("-13");
+            $response->setMessage('User has no RBAC permissions to access the data.');
+        }
+        $response->toJSON();
     });
 });
