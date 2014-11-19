c69470cbfaddb54d2814c6293eb9e33424d6cee2
Dirk Schäfer <schaefer@hrz.uni-marburg.de>
2014-11-19 13:39:01 +0100

    Revision of the Describr API

--- RESTController/extensions/describe_v1/routes/inc.ilDescribrRoutes.php
+++ RESTController/extensions/describe_v1/routes/inc.ilDescribrRoutes.php
@@ -14,16 +14,15 @@ $app->group('/admin', function () use ($app) {
      * Supported types: obj_id, ref_id, usr_id and file_id
      */
     $app->get('/describe/:id', 'authenticateILIASAdminRole', function ($id) use ($app) {
+        $request = new ilRestRequest($app);
+        $response = new ilRestResponse($app);
 
-
-        $id_type = $app->request()->params("id_type"); // ref_id, obj_id, usr_id
-        if (!defined($id_type)) {
+        try {
+            $id_type = $request->getParam('id_type');
+        } catch (Exception $e) {
             $id_type = 'ref_id';
         }
 
-
-       // echo "iD_type =".$id_type;
-        $result = array();
         $model = new ilDescribrModel();
         if ($id_type == 'ref_id' || $id_type == 'obj_id') {
             if ($id_type == 'ref_id') {
@@ -32,13 +31,21 @@ $app->group('/admin', function () use ($app) {
             }
             //echo "obj_id:".$obj_id;
             try {
-                if (is_numeric($obj_id)==false) {
+                if (is_numeric($obj_id) == false) {
                     throw new Exception('Obj id does not exist');
                 }
                 $a_descr = $model->describeIliasObject($obj_id);
-                $result['object_description'] = $a_descr;
+                $response->addData('object_description', $a_descr);
+                $response->setMessage('Object found.');
+
+                if ($a_descr['type'] == "file") {
+                    $id = $obj_id;
+                    $id_type = "file_id";
+                }
             } catch (Exception $e) {
-                $result['error'] = 'Object not found.';// Try to explain a user with id = '.$id.' instead.';
+                $response->setRestCode('-11');
+                $response->setMessage('Error: Object not found.');
+                // Try to explain a user with id = '.$id.' instead.';
                 $id_type = 'usr_id';
             }
         }
@@ -48,21 +55,23 @@ $app->group('/admin', function () use ($app) {
             //echo $username;
             try {
                 if ($username == 'User unknown') {
-                    $result['msg'] = 'User not found.';
+                    $response->setMessage('User not found.');
                     throw new Exception('User does not exist');
                 } else {
                     ilRestLib::initDefaultRestGlobals();
                     $usr_model = new ilUsersModel();
                     $usr_basic_info =  $usr_model->getBasicUserData($id);
                     if (empty($usr_basic_info) == true) {
-                        $result['msg'] = 'User not found.';
+                        $response->setMessage('Error: User not found.');
                     } else {
-                        $result['msg'] = 'User found.';
-                        $result['user'] = $usr_basic_info;
+                        $response->setMessage('User  found.');
+                        $response->addData('user', $usr_basic_info);
                     }
                 }
             } catch (Exception $e) {
-                $result['error'] = $result['error'].'User not found.';// Try to explain a file with id = '.$id.' instead.';
+                $response->setRestCode('-11');
+                $response->setMessage('Error: User not found.');
+                // Try to explain a file with id = '.$id.' instead.';
                 $id_type = 'file_id';
             }
         }
@@ -70,18 +79,14 @@ $app->group('/admin', function () use ($app) {
         if ($id_type == 'file_id') {
             try {
                 $data = $model->describeFile($id);
-                $result['msg'] = 'Description of file with id = '.$id;
-                $result['file'] = $data;
+                $response->addMessage('Description of file with id = '.$id.'.');
+                $response->addData('file', $data);
             } catch (Exception $e) {
-                $result['error'] = $result['error'].'File not found.';
+                $response->setRestCode('-11');
+                $response->setMessage('Error: File not found.');
             }
         }
-
-        $app->response()->header('Content-Type', 'application/json');
-        echo json_encode($result);
-
+        $response->send();
     });
-
-
 
 });
