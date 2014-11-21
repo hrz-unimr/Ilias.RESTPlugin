1897378387647edc57594db86f5ce9f5dffaa1b3
Dirk Schäfer <schaefer@hrz.uni-marburg.de>
2014-11-21 11:26:26 +0100

    (Un-)subscribe course functionality added

---
+++ RESTController/extensions/courses_v1/models/class.ilCourseRegistrationModel.php
@@ -0,0 +1,244 @@
+<?php
+require_once "./Services/Utilities/classes/class.ilUtil.php";
+require_once "./Modules/Course/classes/class.ilObjCourse.php";
+require_once './Services/Object/classes/class.ilObjectFactory.php';
+require_once './Services/Object/classes/class.ilObjectActivation.php';
+require_once("./Modules/LearningModule/classes/class.ilObjLearningModule.php");
+require_once("./Modules/LearningModule/classes/class.ilLMPageObject.php");
+require_once "./Services/Database/classes/class.ilDB.php";
+require_once "./Services/Database/classes/class.ilAuthContainerMDB2.php";
+//require_once "./Modules/Course/classes/class.ilCourseConstants.php";
+
+class ilCoursesRegistrationModel
+{
+    private $waiting_list;
+    private $participants;
+    private $container;
+
+    /**
+     * Subscribes a user to a course.
+     *
+     * @param user_id - the id of a user
+     * @param ref_id - ref_id of a course object
+     */
+    public function joinCourse($user_id, $ref_id)
+    {
+
+        ilRestLib::initDefaultRestGlobals();
+
+        $this->user_id = $user_id;
+        $this->ref_id = $ref_id;
+        $this->obj_id = ilRestLib::refid_to_objid($ref_id);
+        $this->container = ilObjectFactory::getInstanceByRefId($this->ref_id, false);
+
+        ilRestLib::initGlobal("ilUser", "ilObjUser", "./Services/User/classes/class.ilObjUser.php");
+        global $ilUser;
+        $ilUser->setId($user_id);
+        $ilUser->read();
+        ilRestLib::initAccessHandling();
+
+        $this->initParticipants();
+        $this->initWaitingList();
+        if ($this->checkSubscribeConditions() == true) {
+            $this->add();
+        } else {
+            //  "user on waiting list!";
+        }
+    }
+
+    /**
+     * Unsubscribes (or removes) a user from a course
+     *
+     * @param user_id - the id of a user
+     * @param ref_id - ref_id of a course object
+     */
+    public function leaveCourse($user_id, $ref_id)
+    {
+        ilRestLib::initDefaultRestGlobals();
+        $this->container = ilObjectFactory::getInstanceByRefId($ref_id, false);
+
+        ilRestLib::initGlobal("ilUser", "ilObjUser", "./Services/User/classes/class.ilObjUser.php");
+        global $ilUser;
+        $ilUser->setId($user_id);
+        $ilUser->read();
+        ilRestLib::initAccessHandling();
+        if ($this->checkUnsubscribeConditions() == false) {
+            echo "User cannot leave the course, because she is the last course admin.";
+        } else {
+            // proceed
+            $this->performUnsubscribeObject();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /* Find methods regarding the subscription of courses below */
+
+    /**
+     * see original methods at class.ilCourseRegistrationGUI.php
+     */
+    private function checkSubscribeConditions()
+    {
+        global $ilUser;
+        if($this->waiting_list->isOnList($ilUser->getId())) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Init course participants
+     *
+     * @access protected
+     */
+    private function initParticipants()
+    {
+        include_once('./Modules/Course/classes/class.ilCourseParticipants.php');
+        $this->participants = ilCourseParticipants::_getInstanceByObjId($this->container->getId());//$this->obj_id);
+    }
+
+
+    /**
+     * @see ilRegistrationGUI::initWaitingList()
+     * @access protected
+     */
+    private function initWaitingList()
+    {
+        include_once './Modules/Course/classes/class.ilCourseWaitingList.php';
+        $this->waiting_list = new ilCourseWaitingList($this->container->getId()); //$this->obj_id);
+    }
+
+    /**
+     * add user
+     *
+     * @access protected
+     * @param
+     * @return
+     */
+    private function add()
+    {
+        global $ilUser,$tree, $ilCtrl;
+
+        // set aggreement accepted
+        $this->setAccepted(true);
+
+        include_once('./Modules/Course/classes/class.ilCourseWaitingList.php');
+        $free = max(0,$this->container->getSubscriptionMaxMembers() - $this->participants->getCountMembers());
+        $waiting_list = new ilCourseWaitingList($this->container->getId());
+        if($this->container->isSubscriptionMembershipLimited() and $this->container->enabledWaitingList() and (!$free or $waiting_list->getCountUsers()))
+        {
+            $waiting_list->addToList($ilUser->getId());
+            $info = sprintf($this->lng->txt('crs_added_to_list'),
+                $waiting_list->getPosition($ilUser->getId()));
+            ilUtil::sendSuccess($info,true);
+
+            $this->participants->sendNotification($this->participants->NOTIFY_SUBSCRIPTION_REQUEST,$ilUser->getId());
+            $this->participants->sendNotification($this->participants->NOTIFY_WAITING_LIST,$ilUser->getId());
+            $ilCtrl->setParameterByClass("ilrepositorygui", "ref_id",
+                $tree->getParentId($this->container->getRefId()));
+            $ilCtrl->redirectByClass("ilrepositorygui", "");
+        }
+
+        switch($this->container->getSubscriptionType())
+        {
+            case IL_CRS_SUBSCRIPTION_CONFIRMATION:
+                $this->participants->addSubscriber($ilUser->getId());
+                $this->participants->updateSubscriptionTime($ilUser->getId(),time());
+                $this->participants->updateSubject($ilUser->getId(),ilUtil::stripSlashes($_POST['subject']));
+                $this->participants->sendNotification($this->participants->NOTIFY_SUBSCRIPTION_REQUEST,$ilUser->getId());
+
+                ilUtil::sendSuccess($this->lng->txt("application_completed"),true);
+                $ilCtrl->setParameterByClass("ilrepositorygui", "ref_id",
+                    $tree->getParentId($this->container->getRefId()));
+                $ilCtrl->redirectByClass("ilrepositorygui", "");
+                break;
+
+            default:
+
+                if($this->container->isSubscriptionMembershipLimited() && $this->container->getSubscriptionMaxMembers())
+                {
+                    $success = $GLOBALS['rbacadmin']->assignUserLimited(
+                        ilParticipants::getDefaultMemberRole($this->container->getRefId()),
+                        $ilUser->getId(),
+                        $this->container->getSubscriptionMaxMembers(),
+                        array(ilParticipants::getDefaultMemberRole($this->container->getRefId()))
+                    );
+                    if(!$success)
+                    {
+                        // The maximum number of participants has been exceeded.
+                        //ilUtil::sendFailure($this->lng->txt('crs_subscription_failed_limit'));
+                        return FALSE;
+                    }
+                }
+
+                $this->participants->add($ilUser->getId(),IL_CRS_MEMBER);
+                $this->participants->sendNotification($this->participants->NOTIFY_ADMINS,$ilUser->getId());
+                $this->participants->sendNotification($this->participants->NOTIFY_REGISTERED,$ilUser->getId());
+
+                include_once './Modules/Forum/classes/class.ilForumNotification.php';
+                ilForumNotification::checkForumsExistsInsert($this->container->getRefId(), $ilUser->getId());
+
+                if($this->container->getType() == "crs")
+                {
+                    $this->container->checkLPStatusSync($ilUser->getId());
+                }
+
+                // You have joined the course
+                return TRUE;
+                break;
+        }
+    }
+
+    /**
+     * (Cloned from Membership/classes/class.ilRegistrationGUI.php)
+     * Set Agreement accepted
+     *
+     * @access private
+     * @param bool
+     */
+    private function setAccepted($a_status)
+    {
+        global $ilUser;
+
+        /*include_once('Modules/Course/classes/Export/class.ilCourseDefinedFieldDefinition.php');
+        if(!$this->privacy->confirmationRequired($this->type) and !ilCourseDefinedFieldDefinition::_hasFields($this->container->getId()))
+        {
+            return true;
+        }
+        */
+        include_once('Services/Membership/classes/class.ilMemberAgreement.php');
+        $this->agreement = new ilMemberAgreement($ilUser->getId(),$this->container->getId());
+        $this->agreement->setAccepted($a_status);
+        $this->agreement->setAcceptanceTime(time());
+        $this->agreement->save();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /* Find methods regarding the unsubscription of courses below */
+
+    /**
+     * see original class.ilObjCourseGUI.php > leaveObject
+     */
+    private function checkUnsubscribeConditions()
+    {
+        global $ilUser;
+        //$this->checkPermission('leave');
+        if($this->container->getMembersObject()->isLastAdmin($ilUser->getId()) == true) {
+            //ilUtil::sendFailure($this->lng->txt('crs_min_one_admin')); // "There has to be at least one administrator assigned to this course."
+            return false;
+        }
+        return true;
+    }
+
+    private function performUnsubscribeObject()
+    {
+        global $ilUser, $ilCtrl;
+        // CHECK ACCESS
+       // $this->checkPermission('leave');
+        $this->container->getMembersObject()->delete($ilUser->getId());
+        $this->container->getMembersObject()->sendUnsubscribeNotificationToAdmins($ilUser->getId());
+        $this->container->getMembersObject()->sendNotification($this->container->getMembersObject()->NOTIFY_UNSUBSCRIBE,$ilUser->getId());
+        //ilUtil::sendSuccess($this->lng->txt('crs_unsubscribed_from_crs'),true); // "You have been unsubscribed from this course"
+    }
+
+}
--- RESTController/extensions/courses_v1/routes/inc.ilCourseRoutes.php
+++ RESTController/extensions/courses_v1/routes/inc.ilCourseRoutes.php
@@ -7,10 +7,42 @@
 $app->group('/experimental', function () use ($app) {
 
     $app->get('/courses/:ref_id', function ($ref_id) use ($app) {
+        $response = new ilRestResponse($app);
+        $authorizedUserId =  ilRestLib::loginToUserId($env['user']);
         try {
-            $app = \Slim\Slim::getInstance();
-            $env = $app->environment();
+            $crs_model = new ilCoursesModel();
+            $data1 =  $crs_model->getCourseContent($ref_id);
+            $data2 =  $crs_model->getCourseInfo($ref_id);
+            $response->addData('coursecontents', $data1);
+            $response->addData('courseinfo', $data2);
+            $response->setMessage("Content of course " . $ref_id . ".");
+        } catch (Exception $e) {
+            $response->setRestCode("-15");
+            $response->setMessage('Error: Could not retrieve data for user '.$id.".");
+        }
+        $response->toJSON();
+        /*if ($authorizedUserId == $id || ilRestLib::isAdmin($authorizedUserId)) { // only the user or the admin is allowed to access the data
+            try {
+                $crs_model = new ilCoursesModel();
+                $data1 =  $crs_model->getCourseContent($ref_id);
+                $data2 =  $crs_model->getCourseInfo($ref_id);
+                $response->addData('course_description', $data1);
+                $response->addData('course_content', $data2);
+                $response->setMessage("Courses of user " . $id . ".");
+            } catch (Exception $e) {
+                $response->setRestCode("-15");
+                $response->setMessage('Error: Could not retrieve data for user '.$id.".");
+            }
+        } else {
+            $response->setRestCode("-13");
+            $response->setMessage('User has no RBAC permissions to access the data.');
+        }
+        $response->toJSON();
+        */
 
+
+       /* try {
+            $env = $app->environment();
             $result = array();
             // $result['usr_id'] = $user_id;
             $crs_model = new ilCoursesModel();
@@ -25,11 +57,10 @@ $app->group('/experimental', function () use ($app) {
         } catch (Exception $e) {
             $app->response()->status(400);
             $app->response()->header('X-Status-Reason', $e->getMessage());
-        }
+        }*/
     });
 
     $app->post('/courses',  function() use ($app) {
-        $app = \Slim\Slim::getInstance();
         $env = $app->environment();
 
         $parent_container_ref_id = 1;
@@ -51,7 +82,6 @@ $app->group('/experimental', function () use ($app) {
             $new_course_title = $request->post('title');
             $new_course_description = $request->post('description');
         }
-
         $result = array();
         // $result['usr_id'] = $user_id;
         $crs_model = new ilCoursesModel();
@@ -59,22 +89,14 @@ $app->group('/experimental', function () use ($app) {
         $new_ref_id =  $crs_model->createNewCourseAsUser($user_id, $parent_container_ref_id, $new_course_title, $new_course_description);
         $result['msg'] = "Created a new course with ref id ".$new_ref_id.". Parent ref_id: ".$parent_container_ref_id;
 
-
         $app->response()->header('Content-Type', 'application/json');
         echo json_encode($result);
-
-
     });
 
-
-
     $app->delete('/courses/:id',  function ($id) use ($app) {
-        $app = \Slim\Slim::getInstance();
         $request = $app->request();
         $env = $app->environment();
-
         // todo: check permissions
-
         $result = array();
         $crs_model = new ilCoursesModel();
         $soap_result = $crs_model->deleteCourse($id);
@@ -83,7 +105,49 @@ $app->group('/experimental', function () use ($app) {
         $result['soap_result'] = $soap_result;
         $app->response()->header('Content-Type', 'application/json');
         echo json_encode($result);
+    });
 
+
+    $app->get('/join', 'authenticate', function () use ($app) {
+        $env = $app->environment();
+        $response = new ilRestResponse($app);
+        $request = new ilRestRequest($app);
+        $authorizedUserId =  ilRestLib::loginToUserId($env['user']);
+        try {
+            $ref_id = $request->getParam("ref_id");
+            $crsreg_model = new ilCoursesRegistrationModel();
+            $crsreg_model->joinCourse($authorizedUserId, $ref_id);
+            /*$data1 =  $crs_model->getCourseContent($ref_id);
+            $data2 =  $crs_model->getCourseInfo($ref_id);
+            $response->addData('coursecontents', $data1);
+            $response->addData('courseinfo', $data2);*/
+            $response->setMessage("User ".$authorizedUserId." subscribed to course with ref_id = " . $ref_id . " successfully.");
+        } catch (Exception $e) {
+            $response->setRestCode("-15");
+            $response->setMessage("Error: Subscribing user ".$authorziedUserid." to course with ref_id = ".$ref_id." failed. Exception:".$e);
+            //$response->setMessage('Error: Could not perform action for user '.$id.".".$e);
+            $response->setMessage($e);
+        }
+        $response->toJSON();
+    });
+
+    $app->get('/leave', 'authenticate', function () use ($app) {
+        $env = $app->environment();
+        $response = new ilRestResponse($app);
+        $request = new ilRestRequest($app);
+        $authorizedUserId =  ilRestLib::loginToUserId($env['user']);
+        try {
+            $ref_id = $request->getParam("ref_id");
+            $crsreg_model = new ilCoursesRegistrationModel();
+            $crsreg_model->leaveCourse($authorizedUserId, $ref_id);
+
+            $response->setMessage("User ".$authorizedUserId." has left course with ref_id = " . $ref_id . ".");
+        } catch (Exception $e) {
+            $response->setRestCode("-15");
+            $response->setMessage('Error: Could not perform action for user '.$authorizedUserId.".".$e);
+            $response->setMessage($e);
+        }
+        $response->toJSON();
     });
 
 
