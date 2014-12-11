99ad55199b7847da6b7361e825a72ee78082bc01
Dirk Schäfer <schaefer@hrz.uni-marburg.de>
2014-12-11 14:48:36 +0100

    Moved mobile search endpoint to the mobile group routes.

--- RESTController/extensions/mobile_v1/routes/inc.ilMobileRoutes.php
+++ RESTController/extensions/mobile_v1/routes/inc.ilMobileRoutes.php
@@ -30,80 +30,6 @@ $app->group('/m', function () use ($app) {
 
     });
 
-
-    /**
-     *  Loads the following data:
-     * - User / system information
-     * - Personal courses, which are flagged "online"
-     * - Meta-information about the courses
-     * - Lists of contents of the personal courses // see of contacts > my courses, my groups
-     * - People participating the courses
-     * - Dates (Note: System Caldendar must be activated)
-     *  - List
-     *  - ICAL (ics) Feed Url
-     */
-    $app->get('/desk', function () use ($app) {
-        $result = array();
-
-        // TODO: extract user_id from valid token
-        $user_id = 6;//225;//6;//361; // testuser
-        //$user = ilRestLib::userIdtoLogin($user_id);
-
-        // use case: load all available courses
-        // later: split according to current semesters and
-        // information on courses for older semesters are provided on demand (this should be the default case)
-
-        // the mobile desktop is made up of
-        // courses the user is participating
-        // in addition to the course names also the
-        // list of items for each course is sent, to enable better
-        // UI interactions
-        ilRestLib::initDefaultRestGlobals();
-        ilRestLib::initAccessHandling();
-
-        $userModel = new ilUsersModel();
-        $userData = $userModel->getBasicUserData($user_id);
-        $result['user'] = $userData;
-
-        $courseModel = new ilCoursesModel();
-        $course_list = $courseModel->getCoursesOfUser($user_id);
-
-        $course_contents = array();
-        $course_info = array();
-        foreach ($course_list as $course_refid)
-        {
-            $course_contents[$course_refid] = $courseModel->getCourseContent($course_refid);
-            $children_ref_ids = array();
-            foreach ($course_contents[$course_refid] as $item) {
-                $children_ref_ids[] = $item['ref_id'];
-            }
-            //var_dump($children_ref_ids);
-            $course_item = $courseModel->getCourseInfo($course_refid);
-            $course_item['children_ref_ids'] = $children_ref_ids;
-            $course_info[$course_refid] = $course_item;
-
-            //var_dump($course_info[$course_refid]);
-            $course_info[$course_refid]['content_length']= count($course_contents[$course_refid]);
-        }
-        $result['courses'] = $course_info;
-        $result['contents'] = $course_contents;
-
-        // Calendar
-        $calModel = new ilCalendarModel();
-        $data = $calModel->getIcalAdress($user_id);
-        $result['ical_url'] = $data;
-        $data = $calModel->getCalUpcomingEvents($user_id);
-        $result['events'] = $data;
-
-        // Contacts
-        $contactModel = new ilContactsModel();
-        $data = $contactModel->getMyContacts($user_id);
-        $result['contacts']['mycontacts'] = $data;
-
-        $result['status'] = "ok";
-        echo json_encode($result);
-    });
-
     /**
      *  Loads the following data:
      * - User / system information
@@ -116,7 +42,7 @@ $app->group('/m', function () use ($app) {
      *  - ICAL (ics) Feed Url
      *  In contrast to version 1, the json is structured in a better and more concise way.
      */
-    $app->get('/desk2', function () use ($app) {
+    $app->get('/desk', function () use ($app) {
         $t_start = microtime();
         $result = array();
 
@@ -181,4 +107,52 @@ $app->group('/m', function () use ($app) {
         $result['status']['tstamp'] = time();
         echo json_encode($result);
     });
+
+    $app->get('/search/',  function () use ($app) {
+        $request = new ilRestRequest($app);
+        $response = new ilRestResponse($app);
+
+        try {
+            $query = $request->getParam('q');
+        } catch (Exception $e) {
+            $query = '';
+        }
+
+        // Using anonymous function PHP 5.3.0>=
+        spl_autoload_register(function($class){
+            if (file_exists($_SERVER['DOCUMENT_ROOT'].REST_PLUGIN_DIR.'/RESTController/extensions/mobile_v1/addon/' . $class . '.php')) {
+                require_once($_SERVER['DOCUMENT_ROOT'].REST_PLUGIN_DIR.'/RESTController/extensions/mobile_v1/addon/' . $class . '.php');
+            }
+        });
+
+        // Using elastica 1.3.4 (corresponding to elastic search 1.3.4)
+        $elasticaClient = new \Elastica\Client();
+        $esQuery = '{
+            "query": {
+                "fuzzy_like_this" : {
+                    "fields" : ["title"],
+                    "like_text" : "'.$query.'",
+                    "max_query_terms" : 25
+                }
+            }
+        }';
+        $path = 'jdbc' . '/_search';
+
+        $esResponse = $elasticaClient->request($path, \Elastica\Request::POST, $esQuery);
+        $esResponseArray = $esResponse->getData();
+        $searchResults = array();
+        foreach ($esResponseArray['hits']['hits'] as $hit) {
+            $searchResults[] = array('obj_id' => $hit['_source']['obj_id'],
+                'type' => $hit['_source']['type'],
+                'title' => $hit['_source']['title'],
+                'age' => $hit['_source']['ageindays'],
+                'score' => $hit['_score']);
+        }
+
+        $response->addData('search_results', $searchResults);
+        $response->setMessage('You have been searching for: "'.$query.'"');
+        $response->send();
+    });
+
+
 });
