c6a59bf85480d526c737adb4b4c7604ec400238d
Dirk Schäfer <schaefer@hrz.uni-marburg.de>
2015-01-13 11:37:10 +0100

    Tools for REST

---
+++ tools/ilias-shell/ishell.py
@@ -0,0 +1,67 @@
+import ConfigParser
+import json
+import urllib
+import urllib2
+
+class IShell:
+   """	
+		ILIAS-Shell
+		Enables personalized and administrative operations on the ILIAS LMS.
+		v.1.0
+   """
+
+   def __init__(self):
+		settings = ConfigParser.ConfigParser();
+		settings.read('ishell.ini')
+		self.api_key = settings.get('Authentification','api_key')
+		self.username = settings.get('Authentification','username')
+		self.userpass = settings.get('Authentification','password')
+		self.oauth2_endpoint = settings.get('Authentification','oauth2_endpoint')
+		self.ilias_client_id = settings.get('System','ilias_client_id')
+		self.rest_endpoint = settings.get('System','rest_endpoint')
+		self.connect()
+
+   def connect(self):
+		params = urllib.urlencode({'grant_type': 'password', #Invoke OAuth2 mechanism via "resources owner credentials".
+		'username': self.username,
+		'password': self.userpass,
+		'client_id': self.api_key,
+		'ilias_client_id' : self.ilias_client_id
+		})
+		response = urllib2.urlopen(self.rest_endpoint+self.oauth2_endpoint, params).read()
+		data = json.loads(response)
+		self.token = data['access_token']
+		#f = open('token', 'w')
+		#f.write('header="Authorization: Bearer ' + tokenstr + '"')
+		#f.close()
+		print 'Welcome to the ILIAS-Shell'
+		print 'Connected successfully to ' + self.rest_endpoint + self.oauth2_endpoint
+		print 'Retrieved OAuth2 Token: ' + self.token
+
+   def getRoutes(self):
+		response = urllib2.urlopen(self.rest_endpoint+'/routes').read()
+		data = json.loads(response)
+		self.latestResponse = data
+		return data
+
+   def show(self):
+		 print(json.dumps(self.latestResponse, indent=2))
+		 
+   def dump(self, jsondata):
+		f = open('dump.json','w')
+		f.write(json.dumps(jsondata, indent=2))
+		f.close
+	
+   def get(self, routeStr):
+		#response = urllib2.urlopen(self.rest_endpoint+'/'+routeStr).read()
+		request = urllib2.Request(self.rest_endpoint+'/'+routeStr)
+		request.add_header('Authorization', ''+ self.token)
+		response = urllib2.urlopen(request).read()
+		#response
+		data = json.loads(response)
+		self.latestResponse = data
+		return data
+
+   def users(self):
+		self.get('/v1/users');
+		self.show();
---
+++ tools/ilias-shell/readme.txt
@@ -0,0 +1,17 @@
+ILIAS-Shell
+-----------
+The ILIAS-Shell is useful for performing personalized and administrative operations based on IPython.
+D.Schaefer <schaefer@hrz.uni-marburg>, 2015
+
+Install:
+Just configure the file ishell.py and start by typing ipython on the command line as described below.
+
+Example:
+$ ipython
+$ import ishell
+$ il = ishell.IShell()
+
+From there on you can invoke further commands.
+$ routes=il.getRoutes()
+$ il.get('/admin/describe/64');
+$ il.show()
