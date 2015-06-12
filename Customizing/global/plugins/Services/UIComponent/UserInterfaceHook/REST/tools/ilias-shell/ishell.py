5700c25c06d0afd2297c49e11c5d5e7d3e224c29
disc5 <ds@jivas.de>
2015-06-12 10:42:01 +0200

    IShell v.1.2

--- Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/tools/ilias-shell/ishell.py
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/tools/ilias-shell/ishell.py
@@ -7,7 +7,7 @@ class IShell:
     """  
     ILIAS-Shell
     Enables personalized and administrative operations on the ILIAS LMS.
-    v.1.1
+    v.1.2
     """
 
     def __init__(self):
@@ -22,15 +22,23 @@ class IShell:
         self.connect()
 
     def getToken(self):
-        params = urllib.urlencode({'grant_type': 'password', #Invoke OAuth2 mechanism via "resources owner credentials".
-              'api_key': self.api_key,
+        #print '[Debug] In get Token'
+	#print self.username
+	#print self.userpass
+	#print self.api_key
+	#print self.ilias_client_id
+	#print self.rest_endpoint
+	#print self.oauth2_endpoint
+	params = urllib.urlencode({'grant_type': 'password', 	#Invoke OAuth2 mechanism via "resources owner credentials".
+        'api_key': self.api_key,
         'username': self.username,
         'password': self.userpass,
         'client_id': self.api_key,
         'ilias_client_id' : self.ilias_client_id
         })
         response = urllib2.urlopen(self.rest_endpoint+self.oauth2_endpoint, params).read()
-        data = json.loads(response)
+        #print response
+	data = json.loads(response)
         self.token = data['access_token']
 
 
@@ -45,17 +53,17 @@ class IShell:
    
     def printIntro(self):
         print '--'
-        print 'Hint: to get started just type $il. + <tab> to see some functions.'
-        print 'Example: $il.getRoutes(); followed by $il.show()'
+        print 'Hint: to get started just typ il.<tab> to see some functions.'
+        print 'Example: il.getRoutes(), see variable il.response afterwards'
    
     def getRoutes(self):
         response = urllib2.urlopen(self.rest_endpoint+'/routes').read()
         data = json.loads(response)
-        self.latestResponse = data
-        return data
+        self.response = data
+        self.show()
 
     def show(self):
-        print(json.dumps(self.latestResponse, indent=2))
+        print(json.dumps(self.response, indent=2))
      
     def dump(self, jsondata):
         f = open('dump.json','w')
@@ -63,30 +71,58 @@ class IShell:
         f.close
   
     def post(self, routeStr, data):
-#response = urllib2.urlopen(self.rest_endpoint+'/'+routeStr).read()
-        request = urllib2.Request(self.rest_endpoint+'/'+routeStr)
+        if routeStr[0]=='/':
+                routeStr = routeStr[1:]
+	request = urllib2.Request(self.rest_endpoint+'/'+routeStr)
         request.add_header('Authorization', ' Bearer '+ self.token)
         enc_data = urllib.urlencode(data)
         response = urllib2.urlopen(url=request, data=enc_data).read()
-#response
         data = json.loads(response)
-        self.latestResponse = data
+        self.response = data
         return data
 
-    def get(self, routeStr, firstAttempt=True):
-#response = urllib2.urlopen(self.rest_endpoint+'/'+routeStr).read()
+    def get(self, routeStr):
+	if routeStr[0]=='/':
+		routeStr = routeStr[1:]
         request = urllib2.Request(self.rest_endpoint+'/'+routeStr)
         request.add_header('Authorization', ' Bearer '+ self.token)
         response = urllib2.urlopen(request).read()
-#response
+	#print response
         data = json.loads(response)
-        if data['msg'] == 'Token expired.' and firstAttempt:
-            self.getToken()
-            return self.get(routeStr, False)
-        else: 
-            self.latestResponse = data
-            return data
+	#print data
+        # todo: should be "msg"
+	#if data['status'] == 'Token expired.' and firstAttempt:
+        #    self.getToken()
+        #    return self.get(routeStr, False)
+        #else: 
+        self.response = data
+	self.show()
+
+    def delete(self, routeStr):
+	if routeStr[0]=='/':
+                routeStr = routeStr[1:]
+        request = urllib2.Request(self.rest_endpoint+'/'+routeStr)
+        request.add_header('Authorization', ' Bearer '+ self.token)
+        request.get_method = lambda: 'DELETE'
+	response = urllib2.urlopen(url=request).read()
+        #opener = urllib2.build_opener(urllib2.HTTPHandler)
+	data = json.loads(response)
+        self.response = data
+	self.show()
+
+    def put(self, routeStr, data):
+        if routeStr[0]=='/':
+                routeStr = routeStr[1:]
+        request = urllib2.Request(self.rest_endpoint+'/'+routeStr)
+        request.add_header('Authorization', ' Bearer '+ self.token)
+        request.get_method = lambda: 'PUT'
+	enc_data = urllib.urlencode(data)
+        response = urllib2.urlopen(url=request, data=enc_data).read()
+        data = json.loads(response)
+        self.response = data
+	self.show()
 
     def users(self):
         self.get('/v1/users');
         self.show();
+
