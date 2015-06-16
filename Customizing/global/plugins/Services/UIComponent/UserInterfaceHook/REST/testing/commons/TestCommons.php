bcb7a3e67ad7596cf441369711535b00ebae40a6
disc5 <ds@jivas.de>
2015-06-16 13:46:39 +0200

    API-Testing: added test commons

---
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/testing/commons/TestCommons.php
@@ -0,0 +1,21 @@
+<?php
+class TestCommons
+{
+    public static $username = 'root';
+    public static $password = 'homer';
+    public static $api_key = 'apollon';
+    public static $token = '';
+
+    public static function logMeIn($I)
+    {
+        $I->wantTo('authenticate via oauth2 user credentials');
+        $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $aPost = array('grant_type' => 'password',
+            'username' => TestCommons::$username,
+            'password' => TestCommons::$password,
+            'api_key' => TestCommons::$api_key);
+        $I->sendPOST('v1/oauth2/token',$aPost);
+        TestCommons::$token = $I->grabDataFromResponseByJsonPath('$.access_token')[0];
+
+    }
+}
