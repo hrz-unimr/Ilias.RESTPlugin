3133a335a96a4a95247f7bb78fd213195408b46c
disc5 <ds@jivas.de>
2016-04-20 09:44:05 +0200

    Cleanup /v1/contacts

--- Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/RESTController/extensions/contacts_v1/routes/ContactsRoutes.php
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/RESTController/extensions/contacts_v1/routes/ContactsRoutes.php
@@ -10,11 +10,33 @@ namespace RESTController\extensions\contacts_v1;
 // This allows us to use shortcuts instead of full quantifier
 use \RESTController\libs\RESTAuth as RESTAuth;
 use \RESTController\libs as Libs;
-use \RESTController\core\auth as Auth;
 use \RESTController\libs\Exceptions as LibExceptions;
 
 
 $app->group('/v1', function () use ($app) {
+
+    /**
+     * Returns the personal ILIAS contacts of the authenticated user.
+     */
+    $app->get('/contacts', RESTAuth::checkAccess(RESTAuth::PERMISSION), function () use ($app) {
+        $accessToken = $app->request->getToken();
+
+
+        $authorizedUserId =  $accessToken->getUserId();
+
+        if ($authorizedUserId > -1) { // only the user is allowed to access the data
+            $id = $authorizedUserId;
+
+            $model = new ContactsModel();
+            $data = $model->getMyContacts($id);
+            $app->success($data);
+
+        }
+        else {
+            $app->halt(401, Libs\OAuth2Middleware::MSG_NO_ADMIN, Libs\OAuth2Middleware::ID_NO_ADMIN);
+        }
+    });
+
     /**
      * Returns the personal ILIAS contacts for a user specified by id.
      */
@@ -34,30 +56,6 @@ $app->group('/v1', function () use ($app) {
         }
         else
             $app->halt(401, Libs\OAuth2Middleware::MSG_NO_ADMIN, Libs\OAuth2Middleware::ID_NO_ADMIN);
-
     });
 
-
-    /**
-     * Returns the personal ILIAS contacts of the authenticated user.
-     */
-    $app->get('/contacts', RESTAuth::checkAccess(RESTAuth::PERMISSION), function () use ($app) {
-       // $accessToken = Auth\Util::getAccessToken();
-        $accessToken = $app->request->getToken();
-
-
-        $authorizedUserId =  $accessToken->getUserId();
-
-        if ($authorizedUserId > -1) { // only the user is allowed to access the data
-            $id = $authorizedUserId;
-
-                $model = new ContactsModel();
-                $data = $model->getMyContacts($id);
-                $app->success($data);
-
-        }
-        else {
-            $app->halt(401, Libs\OAuth2Middleware::MSG_NO_ADMIN, Libs\OAuth2Middleware::ID_NO_ADMIN);
-        }
-    });
 });
