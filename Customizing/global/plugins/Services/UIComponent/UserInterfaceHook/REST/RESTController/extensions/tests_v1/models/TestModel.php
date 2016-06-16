52bb00ed49945fc9b96bc53df1b864bc6a9e670d
disc5 <ds@jivas.de>
2016-06-16 13:30:18 +0200

    Added extension tests_v1 Added routes /v1/tests/download/:ref_id Added routes /v1/tests/:ref_id

---
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/RESTController/extensions/tests_v1/models/TestModel.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * ILIAS REST Plugin for the ILIAS LMS
+ *
+ * Authors: D.Schaefer and T.Hufschmidt <(schaefer|hufschmidt)@hrz.uni-marburg.de>
+ * Since 2014
+ */
+namespace RESTController\extensions\tests_v1;
+
+// This allows us to use shortcuts instead of full quantifier
+use \RESTController\libs as Libs;
+
+require_once('./Services/Utilities/classes/class.ilUtil.php');
+require_once('./Modules/Test/classes/class.ilObjTest.php');
+
+class TestModel extends Libs\RESTModel {
+
+    /**
+     * Downloads a zipped version of a test.
+     * @param $ref_id - reference id of the test
+     * @param $user_id - a user's id
+     */
+    public function getTestExportFile($ref_id, $user_id)
+    {
+        Libs\RESTilias::loadIlUser($user_id);
+        Libs\RESTilias::initAccessHandling();
+        $test = new \ilObjTest($ref_id);
+        $test->read();
+        $xml = $test->getXMLZip();   // "create ZIP and return path"
+        \ilUtil::deliverFile($xml,
+            basename($xml));
+    }
+
+    /**
+     * Returns a list of participants.
+     * @param $ref_id
+     * @param $user_id
+     * @return array
+     */
+    public function getTestParticipants($ref_id, $user_id)
+    {
+        Libs\RESTilias::loadIlUser($user_id);
+        Libs\RESTilias::initAccessHandling();
+        $test = new \ilObjTest($ref_id);
+        $test->read();
+        $participants = $test->getParticipants();
+        return $participants;
+    }
+
+    public function getBasicInformation($ref_id, $user_id)
+    {
+        Libs\RESTilias::loadIlUser($user_id);
+        Libs\RESTilias::initAccessHandling();
+        $test = new \ilObjTest($ref_id);
+        $test->read();
+        return array("title"=>$test->getTitle(),"description"=>$test->getDescription(),"type"=>$type = $test->getType());
+    }
+}
---
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/REST/RESTController/extensions/tests_v1/routes/TestRoutes.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * ILIAS REST Plugin for the ILIAS LMS
+ *
+ * Authors: D.Schaefer, T.Hufschmidt <(schaefer|hufschmidt)@hrz.uni-marburg.de>
+ * Since 2014
+ */
+namespace RESTController\extensions\tests_v1;
+
+// This allows us to use shortcuts instead of full quantifier
+use \RESTController\libs\RESTAuth as RESTAuth;
+use \RESTController\libs as Libs;
+use \RESTController\core\auth as Auth;
+use \RESTController\extensions\tests_v1 as Tests;
+
+$app->group('/v1', function () use ($app) {
+    /**
+     * Downloads a tests specified by its ref_id.
+     */
+    $app->get('/tests/download/:ref_id', RESTAuth::checkAccess(RESTAuth::PERMISSION), function ($ref_id) use ($app) {
+        try {
+            $accessToken = $app->request->getToken();
+            $user_id = $accessToken->getUserId();
+
+            $test_model = new TestModel();
+            $xml =  $test_model->getTestExportFile($ref_id,$user_id);
+
+
+            $app->success(['testFile' => base64_encode(file_get_contents($xml)),
+                'filename' => basename($xml),
+                'fullname' => $xml]);
+
+        } catch (Libs\Exceptions\ReadFailed $e) {
+            $app->halt(500, $e->getFormatedMessage());
+        }
+    });
+
+    /**
+     * Delivers a JSON representation of a test.
+     */
+    $app->get('/tests/:ref_id', RESTAuth::checkAccess(RESTAuth::PERMISSION), function ($ref_id) use ($app) {
+        try {
+            $accessToken = $app->request->getToken();
+            $user_id = $accessToken->getUserId();
+
+            $test_model = new TestModel();
+            $info = $test_model->getBasicInformation($ref_id, $user_id);
+            $participants =  $test_model->getTestParticipants($ref_id, $user_id);
+
+            $result = array(
+                'test' => array($info,$participants)
+            );
+
+            $app->success($result);
+        } catch (Libs\Exceptions\ReadFailed $e) {
+            $app->halt(500, $e->getFormatedMessage());
+        }
+    });
+
+});
