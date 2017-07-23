73c5a38b53f562f7b1a3abc3e3aaec612f2e2b51
Matze <m.stuerner@outlook.de>
2017-07-23 18:02:39 +0200

    Add routes to retrieve all questions of a test/questionpool with answers

--- RESTController/extensions/questionpools_v1/routes/QuestionpoolRoutes.php
+++ RESTController/extensions/questionpools_v1/routes/QuestionpoolRoutes.php
@@ -39,6 +39,48 @@ $app->group('/v1', function () use ($app) {
         }
     });
     /**
+     * Gets the questions of a Questionpool including answers for questions of type 1,2,8. Other types still need to be implemented.
+     * (OPTIONAL) Desired types of questions can be specified in the 'types' parameter of the request 
+     * e.g. "1,2,8" for single choice, multiple choice and text questions only.
+     */
+    $app->get('/questionpools/getQuestionsWithAnswers/:ref_id', RESTAuth::checkAccess(RESTAuth::PERMISSION), function ($ref_id) use ($app) {
+        try {
+            $accessToken = $app->request->getToken();
+            $user_id = $accessToken->getUserId();
+
+            $types = $app->request->getParameter('types','*');
+
+            $questionpool_model = new QuestionpoolModel();
+            $questions = $questionpool_model->getQuestions($ref_id, $user_id, $types);
+
+            //get answers for each question
+            foreach($questions as &$question){
+                switch($question['question_type_fi']){
+                    case 1:
+                        //single choice question
+                        $question['answers'] = $questionpool_model->getSingleChoiceAnswers($question['question_id'], $user_id);
+                        break;
+                    case 2:
+                        //multiple choice question
+                        $question['answers'] = $questionpool_model->getMultipleChoiceAnswers($question['question_id'], $user_id);
+                        break;
+                    case 8:
+                        //text question
+                        $question['answers'] = $questionpool_model->getTextAnswers($question['question_id'], $user_id);
+                        break;
+                }
+            }
+
+            $result = array(
+                'questions' => $questions
+            );
+        
+            $app->success($result);
+        } catch (Libs\Exceptions\ReadFailed $e) {
+            $app->halt(500, $e->getFormatedMessage());
+        }
+    });
+    /**
      * Gets the answers for a question of type 8 = assTextQuestion
      */
     $app->get('/questionpools/getTextAnswers/:ref_id', RESTAuth::checkAccess(RESTAuth::PERMISSION), function ($ref_id) use ($app) {
