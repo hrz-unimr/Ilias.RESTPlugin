90d1d51fa3c36fc0345d3d3fb24984d7aa6fd0e9
Thomas Hufschmidt <hufschmidt@hrz.uni-marburg.de>
2017-10-24 14:23:42 +0200

    Improve user-management routes
    * Implemeted route to search users
    * Dropped unimplemented user routes (profile and list)
    * Local users can now be fetched without parent-object ref-id
    * Refactored user-management (admin) model implementation

--- RESTController/extensions/users_v2/routes/user.php
+++ RESTController/extensions/users_v2/routes/user.php
@@ -10,45 +10,51 @@ namespace RESTController\extensions\users_v2;
 
 // This allows us to use shorter names instead of full namespace quantifier
 // Requires: $app to be \RESTController\RESTController::getInstance();
+use \RESTController\libs          as Libs;
 use \RESTController\libs\RESTAuth as RESTAuth;
 
 
 // Group implemented routes into common group
 //  This routes are more common-user focused rather than administative
 $app->group('/v2/users', function () use ($app) {
-  /**
-   * Todo: Implement route to list all available user accounts, keep returned user-data fields to a minimum though!
-   */
-  $app->get('/list', RESTAuth::checkAccess(RESTAuth::PERMISSION), function () use ($app) {
-    // Note: Return only minimal set of user-data fields
-    $app->halt(501, 'Not yet implemented...');
-  });
-
-
-  /**
-   * Todo: Implement route to search users by certain user-data fields, keep returned user-data fields to a minimum though!
+  /*
+   * Route: [GET] /users/search
+   *  Searches users by certain criteria and returns list of user-ids.
+   *
+   * Parameters:
+   *  search <String> Search string that is partially matched (LIKE %SEARCH%) against in login, firstname, lastname and email
+   *  login <String> Search user by his (exact) login
+   *  external <String> Search user by his (exact) ext_account value (requires authmode too)
+   *  authmode <String> Search ext_account based on given auth-mode
+   *  role <Numeric> Search users by the role (id) they have
+   *  parent <Numeric> Search users by the category or org-unit they are contained within
+   *
+   * Returns:
+   *   <Array[Numeric]> - List of user-ids that match the search-criteria
    */
   $app->get('/search', RESTAuth::checkAccess(RESTAuth::PERMISSION), function () use ($app) {
-    // Note: Return some user-data fields only based on RBAC (& Profile settings)
-    $app->halt(501, 'Not yet implemented...');
-  });
+    try {
+      // Fetch input parameters
+      $request  = $app->request();
+      $search   = $request->getParameter('search',   false);
+      $login    = $request->getParameter('login',    false);
+      $external = $request->getParameter('external', false);
+      $authmode = $request->getParameter('authmode', false);
+      $role     = $request->getParameter('role',     false);
+      $parent   = $request->getParameter('parent',   false);
 
+      // Fetch user-ids
+      $ids = User::SearchUser($search, $login, $external, $authmode, $role, $parent);
 
-  /**
-   * Todo: Implement route to return users profile data
-   */
-  $app->get('/profile/:id', RESTAuth::checkAccess(RESTAuth::PERMISSION), function ($id) use ($app) {
-    // Note: Return some user-data fields only based on Profile settings
-    $app->halt(501, 'Not yet implemented...');
-  });
-
-
-  /**
-   * Todo: Implement route to update users profile data
-   */
-  $app->put('/profile', RESTAuth::checkAccess(RESTAuth::ADMIN), function () use ($app) {
-    // Note: Only allowed to edit onw account/profile
-    $app->halt(501, 'Not yet implemented...');
+      // Return found users
+      $app->success(array(
+        'ids' => $ids,
+      ));
+    }
+    // Catch any exception
+    catch (Libs\RESTException $e) {
+      $e->send();
+    }
   });
 // End of URI group
 });
