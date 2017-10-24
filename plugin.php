df69fc0e25b4aebe4d037eb319337b6b69e48f97
Thomas Hufschmidt <hufschmidt@hrz.uni-marburg.de>
2017-10-24 14:20:35 +0200

    Update plugin requirements (5.0 <= ILIAS <= 5.2.x)

--- plugin.php
+++ plugin.php
@@ -10,8 +10,8 @@
 $id = "rest";
 $version = "1.6.3";
 
-$ilias_min_version = "4.0.0";
-$ilias_max_version = "5.2.0";
+$ilias_min_version = "5.0.0";
+$ilias_max_version = "5.2.99";
 
 $responsible = "D.Schaefer, T.Hufschmidt";
 $responsible_mail = "{schaefer|hufschmidt} at hrz.uni-marburg.de";
